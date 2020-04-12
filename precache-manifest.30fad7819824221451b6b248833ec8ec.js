baa8c777aa0f3634519cda58fada7ff189c8d4b0
Harendra Singh <66hari@gmail.com>
2020-04-12 14:41:39 +0530

    Regenerate artifacts with relative path

---
+++ precache-manifest.30fad7819824221451b6b248833ec8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ef49c75c47099899fbae492db29e1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fe5b5171c0d2a4b30fc",
+    "url": "./static/css/main.2b9e0976.chunk.css"
+  },
+  {
+    "revision": "ba47c0ad2cfee7e3055f",
+    "url": "./static/js/2.2399d111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2399d111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe5b5171c0d2a4b30fc",
+    "url": "./static/js/main.bf7e5d35.chunk.js"
+  },
+  {
+    "revision": "71589b69dbbbb7b3ba7e",
+    "url": "./static/js/runtime-main.3217883d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d90d8ea779ec721c0654ab5188bcfa55.js"
+  "./precache-manifest.30fad7819824221451b6b248833ec8ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
